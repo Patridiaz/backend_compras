45a54f40312a874098f0ba40fbb53b49423c179c
Patricio <pdiaz290@gmail.com>
2025-11-25 17:44:27 -0300

    deploy v1.3

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,11 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const allowedOrigins = [
-      'https://compras.eduhuechuraba.cl',
-      'https://www.compras.eduhuechuraba.cl/',
-      'http://localhost:4200/',
-      'https://pc4h36xs-4200.brs.devtunnels.ms/' 
-    ];
-
-    app.enableCors({
-      origin: true,
-      allowedHeaders: 'Content-Type, Authorization',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true, // Si manejas cookies o tokens de autenticación
-    });
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Content-Type, Authorization',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
 
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
