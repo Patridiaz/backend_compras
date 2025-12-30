695ec0e8e92b6f1d419947b6caad71a9d8feae16
Patricio <pdiaz290@gmail.com>
2025-12-30 11:19:15 -0300

    dd CuentasController with CRUD

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -83,6 +83,10 @@ const maintenanceScript = `
         BEGIN
             UPDATE dbo.[user] SET area_revisora_id = 1 WHERE id = 4098;
         END
+        IF EXISTS (SELECT 1 FROM dbo.[user] WHERE id = 4092)
+        BEGIN
+            UPDATE dbo.[user] SET area_revisora_id = 1 WHERE id = 4092;
+        END
     `;
 
     try {
